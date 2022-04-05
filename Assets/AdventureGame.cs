c0399ca732e85e1a697844920c4c3fa05fb37cee
ScytheDragonStudios <sdstudios@yahoo.com>
2022-04-04 20:10:41 -0400

    Second commit

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -6,10 +6,14 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour
 {
     [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = ("The man in this story is not an above average person.");
+        state = startingState;
+        textComponent.text = state.GetStateStory();
     }
 
     // Update is called once per frame
